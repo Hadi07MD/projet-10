4d208b3318e6fb7054c2851adddf007da65c63ed
Hadi07MD <hadiiisoli94@gmail.com>
2022-03-17 10:32:36 +0100

    edit all

--- Réalisation/delete.php
+++ Réalisation/delete.php
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
+
 <?php
     include "config.php";
     include "userHandler.php";
         if(isset($_GET['id'])){
             $id = $_GET['id'];
             $userHandler = new UserAdd();
-            $userHandler->deleteusers($conn, $id); //:1:id:2:1-2
+            $userHandler->deleteusers($conn, $id); 
             header('Location: index.php');   
     }
-=======
-<?php
-    include "config.php";
-    include "userHandler.php";
-        if(isset($_GET['id'])){
-            $id = $_GET['id'];
-            $userHandler = new UserAdd();
-            $userHandler->deleteusers($conn, $id); //:1:id:2:1-2
-            header('Location: index.php');   
-    }
->>>>>>> 785230eeb077d78d1044191710973bbad471a863
+
 ?>
--- Réalisation/users.php
+++ Réalisation/users.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
     class Users {
         private $id;
@@ -60,67 +60,4 @@
             $this->picture= $value;
         }    
     }
-=======
-<?php
-    class Users {
-        private $id;
-        private $firstName;
-        private $lastName;
-        private $bdate;
-        private $dep;
-        private $salary;
-        private $func;
-        private $picture;
-
-
-        public function getId(){
-            return $this->id;
-        }
-        public function setId($value){
-            $this->id = $value;
-        }
-        public function getFirstName(){
-            return $this->firstName;
-        }
-        public function setFirstName($value){
-            $this->firstName = $value;
-        }
-        public function getLastName(){
-            return $this->lastName;
-        }
-        public function setLastName($value){
-            $this->lastName = $value;
-        }
-        public function getBdate(){
-            return $this->bdate;
-        }
-        public function setBdate($value){
-            $this->bdate= $value;
-        }
-        public function getDep(){
-            return $this->dep;
-        }
-        public function setDep($value){
-            $this->dep= $value;
-        }
-        public function getSalary(){
-            return $this->salary;
-        }
-        public function setSalary($value){
-            $this->salary= $value;
-        }
-        public function getFunc(){
-            return $this->func;
-        }
-        public function setFunc($value){
-            $this->func= $value;
-        }
-        public function getPicture(){
-            return $this->picture;
-        }
-        public function setPicture($value){
-            $this->picture= $value;
-        }    
-    }
->>>>>>> 785230eeb077d78d1044191710973bbad471a863
 ?>
